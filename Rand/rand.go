e02f23426ed626981c7524d42172d20ca17c30b7
terrasdd <reinaldopaixaodev@gmail.com>
2023-02-08 00:18:46 -0400

    estudo

---
+++ Rand/rand.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+)
+
+func main() {
+	fmt.Println("O numero Escolhido é ?", rand.Intn(100))
+	fmt.Println("qual e o valor de pi ?", math.Pi)
+}
