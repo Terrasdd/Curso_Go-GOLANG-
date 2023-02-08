c51a72d097df1f39e43de2cf6fd6fe6ab9a76634
Terrasdd <reinaldopaixaodev@gmail.com>
2023-02-08 16:08:10 -0400

    estudo

--- Funções/funcoes.go
+++ Funções/funcoes.go
@@ -5,6 +5,12 @@ import "fmt"
 func somar(n1 int8, n2 int8) int8 {
 	return n1 + n2
 }
+func calculosMatematicos(n1,n2 int8) (int8, int8){     // permitido ter mais de um retorno por funçao
+	soma := n1 +n2                                      //muito utilizado em tratamento de erro
+	subtracao := n1 -n2
+	return soma, subtracao
+}
+
 func main() {
 	soma := somar(10, 20)
 	fmt.Println(soma)
@@ -18,4 +24,10 @@ func main() {
 
 	f()
 	f2("Texto da funcao ")
+
+	resultadoSoma, resultadoSubtracao := calculosMatematicos(10,15)
+	fmt.Println(resultadoSoma,resultadoSubtracao)
+
+	resultadoSoma2, _ := calculosMatematicos(10,15)       // calcula 2 mais so mostra  um 
+	fmt.Println(resultadoSoma2,)
 }
